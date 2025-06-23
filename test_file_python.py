838b2d5c3b1cd735ccd197586629c17f9862b1ad
Joel Carles-Gonzalez <joel.carles-gonzalez@student-cs.fr>
2025-06-23 17:12:40 +0200

    test git commit main

---
+++ test_file_python.py
@@ -0,0 +1,7 @@
+print("hello is this working?")
+
+def test_function():
+    assert True, "This test should always pass"
+    print("Test function executed successfully.")
+
+print("This is a test file for Python code execution.")
